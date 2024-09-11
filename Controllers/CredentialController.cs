bac6cb1b14d246380da8b24af9a430f8d3942b75
Bavly-Emad <bavlyemad009@gmail.com>
2024-09-11 17:39:42 +0300

    Fix issue with retrieving data.\n Fix Database ConnectionStrings to connect correctly.\n Add [HttpPost], [HttpPut], and [HttpDelete]

--- Controllers/CredentialController.cs
+++ Controllers/CredentialController.cs
@@ -17,10 +17,11 @@ namespace Task1.Controllers
             cntxt = context;
         }
         [HttpGet]
-        public async Task<ActionResult<IEnumerable<Credentials>>> GetCredentials()
+        public async Task<ActionResult<List<Credentials>>> GetCredentials()
         {
             return await cntxt.Credential.ToListAsync();
         }
+
         [HttpGet("{ID}")]
         public async Task<ActionResult<Credentials>> GetCredential(int ID)
         {
@@ -31,6 +32,44 @@ namespace Task1.Controllers
             }
             else
                 return cred;
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<Credentials>> AddCredentials(Credentials cc)
+        {
+            cntxt.Credential.Add(cc);
+            await cntxt.SaveChangesAsync();
+            return Ok(await cntxt.Credential.ToListAsync());
+        }
+
+        [HttpPut]
+        public async Task<ActionResult<Credentials>> UpdateCredentials(Credentials UpdatedCred)
+        {
+            var cred = await cntxt.Credential.FindAsync(UpdatedCred.ID);
+            if (cred == null)
+                return NotFound();
+            else
+            {
+                cred.Name = UpdatedCred.Name;
+                cred.Email = UpdatedCred.Email;
+                cred.Password = UpdatedCred.Password;
+                await cntxt.SaveChangesAsync();
+                return Ok(await cntxt.Credential.ToListAsync());
+            }
+        }
+
+        [HttpDelete]
+        public async Task<ActionResult<Credentials>> DeleteCredentials(int id)
+        {
+            var cred = await cntxt.Credential.FindAsync(id);
+            if (cred == null)
+                return NotFound();
+            else
+            {
+                cntxt.Credential.Remove(cred);
+                await cntxt.SaveChangesAsync();
+                return Ok(await cntxt.Credential.ToListAsync());
+            }
         }
     }
 }
